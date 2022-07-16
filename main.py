7b379f3270876184d3d816f96590edfa5f6aaecc
kattelbiplov <kattelbiplov5@gmail.com>
2022-07-16 15:53:05 +0545

    added post method

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI, Depends
+from requests import session
 from sqlalchemy.orm import Session
 import db
-import models
-
+from models import Person
+from models import UserTable
 
 app = FastAPI()
-
 
 @app.get("/{id}")
 def get_user(id: int, db: Session = Depends(db.get_db)):
     data = db.query(models.UserTBL).filter(models.UserTBL.iduser_id == id).first()
     return {"data": data}
+
+@app.post("/person")
+def add_person(username:str, password:str, email:str, blog_id: int, db:session=Depends(db.get_db)):
+    data = Person(person_username = username, person_password = password, person_email = email, blog_id = blog_id )
+    db.add(data)
+    db.commit()
+    return {"status":201, "message":"Data Added Sucessfully", "data":data}
+
+
+@app.get("/{id}")
+def get_user_by_id(id: int, db: Session = Depends(db.get_db)):
+    data = db.query(Person).filter(Person.person_id == id).first()
+    return {"status":200, "message":"Data Found","data": data}
--- models.py
+++ models.py
@@ -1,13 +1,21 @@
+from email.mime import base
 from sqlalchemy import Column, MetaData, Integer
 from sqlalchemy.sql.sqltypes import NullType
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 metadata = MetaData()
 
-class UserTBL(Base):
+class UserTable(Base):
     __tablename__ = 'user_tbl'
-    iduser_id = Column(Integer, primary_key=True, index=True)
+    iduser_id = Column(Integer, primary_key = True, index = True)
     first_name = Column('first_name', NullType)
-    last_name = Column('last_name', NullType)
+    first_name = Column('last_name', NullType)
     dob = Column('dob', NullType)
+
+class Person(Base):
+    __tablename__ = 'person'
+    person_id = Column(Integer, primary_key = True, index=True)
+    person_username = Column('username', NullType)
+    person_password = Column('password', NullType)
+    person_email = Column('email', NullType)
+    blog_id = Column('blogId')
